40a5cb50ba61a6c4d0fdc6734d2f30a8ba4354c7
linsaftw <linsaftw@users.noreply.github.com>
2023-12-28 12:59:10 -0300

    add contact part

--- elements/modern-navbar.js
+++ elements/modern-navbar.js
@@ -26,6 +26,13 @@ class ModernNavbar extends HTMLElement {
               <!-- Add more social links as needed -->
             </div>
           </li>
+          <li class="dropdown">
+            <a href="#"><i class="fas fa-address-book"></i>Contacto</a>
+            <div class="dropdown-content">
+              <a href="https://wa.me/5491123809742"><i class="fab fa-whatsapp"></i>Whatsapp</a>
+              <a href="mailto:info@agiltech.cc"><i class="fas fa-envelope"></i>Email</a>
+            </div>
+          </li>
         </ul>
       </nav>
       `;
